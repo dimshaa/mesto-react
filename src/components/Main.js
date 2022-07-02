14c6d495affa958424ef241eeddf4830c83c1049
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-07-02 19:53:23 +0300

    add profile data fetching from server

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
+import api from "../utils/Api.js";
+
 function Main(props) {
+
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('');
+
+  useEffect(() => {
+    api.getUserInfo()
+      .then((res) => {
+        setUserAvatar(res.avatar);
+        setUserName(res.name);
+        setUserDescription(res.about);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-wrapper" onClick={props.onEditAvatar}>
-          <img className="profile__avatar" alt="аватар пользователя" />
+          <img className="profile__avatar" src={userAvatar} alt="аватар пользователя" />
         </div>
         <div className="profile__info">
           <div className="profile__info-wrapper">
-            <h1 className="profile__username"></h1>
+            <h1 className="profile__username">{userName}</h1>
             <button className="profile__edit-btn" onClick={props.onEditProfile} type="button" aria-label="редактировать профиль"></button>
           </div>
-          <p className="profile__userbio"></p>
+          <p className="profile__userbio">{userDescription}</p>
         </div>
         <button className="profile__add-btn" onClick={props.onAddPlace} type="button" aria-label="добавить фото"></button>
       </section>
