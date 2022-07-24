d64ed3c3f6cf6deab34cc21f664c1051dcfc5f23
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-07-25 00:30:40 +0300

    delete noValidate atr

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,7 +1,7 @@
 function PopupWithForm(props) {
   return (
     <div className={`popup popup_type_${props.name} ${props.isOpen && "popup_opened"}`}>
-      <form className="popup__form" name={props.name} onSubmit={props.onSubmit} noValidate>
+      <form className="popup__form" name={props.name} onSubmit={props.onSubmit}>
         <h2 className="popup__header">{props.title}</h2>
         {props.children}
         <button className="popup__submit-btn" type="submit">Сохранить</button>
